155cc2122cb69626b52ceb2f71eb559a28a4be38
Nam <nduongbao@ploggmedia.com>
2023-08-07 10:52:42 +0700

    feat: init template

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components';
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+function Home() {
+  return <p>Sample</p>;
+}
+
+export default Home;
